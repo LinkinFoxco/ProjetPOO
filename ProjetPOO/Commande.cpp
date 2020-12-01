f632626ff9a6a3cb5978ef1d338aecd2ae148f73
Mickaël LEBOULANGER <mickael.leboulanger@viacesi.fr>
2020-12-01 16:19:56 +0100

    void Commande::modifier3LVille() Fix

--- ProjetPOO/Commande.cpp
+++ ProjetPOO/Commande.cpp
@@ -25,7 +25,7 @@ void Commande::retirerArticles(Article art)
 void Commande::modifier3LVille()
 {
 	if (m_client.obtenirAdresseLivraison().m_ville.length() - 1 >= 3)
-		m_3Lville = m_client.obtenirAdresseLivraison().m_ville[0] + m_client.obtenirAdresseLivraison().m_ville[1] + m_client.obtenirAdresseLivraison().m_ville[3];
+		m_3Lville = m_client.obtenirAdresseLivraison().m_ville[0] + m_client.obtenirAdresseLivraison().m_ville[1] + m_client.obtenirAdresseLivraison().m_ville[2];
 	else
 		m_3Lville = m_client.obtenirAdresseLivraison().m_ville;
 }
