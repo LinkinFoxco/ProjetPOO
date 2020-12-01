c7b0d26ce06d6813458e65ce02e3ca56af555b99
Tristan ROULET <tristan.roulet@viacesi.fr>
2020-12-01 17:21:54 +0100

    Mapping d'adresse

--- ProjetPOO/Adresse.cpp
+++ ProjetPOO/Adresse.cpp
@@ -1,1 +1,33 @@
 #include "Adresse.h"
+
+Adresse::Adresse(){
+	m_reference = 0;
+	m_numAdresse = 0;
+	m_ville = "";
+	m_codePostal = "";
+	m_rue = "";
+}
+
+Adresse::Adresse(int ref, int num, System::String^ ville, System::String^ codePostal, System::String^ rue){
+	this->m_reference = ref;
+	this->m_numAdresse = num;
+	this->m_ville = ville;
+	this->m_codePostal = codePostal;
+	this->m_rue = rue;
+}
+
+System::String^ Adresse::SELECT(void){
+	return "SELECT ID, Numero_De_Rue, Rue, Code_Postal, Ville FROM Adresse";
+}
+
+System::String^ Adresse::INSERT(void){
+	return "INSERT INTO Adresse (Numero_De_Rue, Rue, Code_Postal, Ville) VALUES('" + this->getNumAdresse() + "', '" + this->getRue() + "', '" + this->getCodePostal() + "', '" + this->getVille() + "');SELECT @@IDENTITY;";
+}
+
+System::String^ Adresse::UPDATE(void){
+	return "UPDATE Adresse SET Numero_De_Rue = '" + this->getNumAdresse() + "', Rue = '" + this->getRue() + "', Code_Postal = '" + this->getCodePostal() + "', Ville = '" + this->getVille() + "' WHERE(ID = " + this->getReference() + ");";
+}
+
+System::String^ Adresse::DELETE(void){
+	return "DELETE FROM Adresse WHERE(ID = " + this->getReference() + ");";
+}
--- ProjetPOO/Adresse.h
+++ ProjetPOO/Adresse.h
@@ -1,13 +1,26 @@
 #pragma once
 #include <iostream>
 
-class Adresse
+ref class Adresse
 {
-public:
+private:
 	int m_reference;
 	int m_numAdresse;
-	std::string m_ville;
-	std::string m_codePostal;
-	std::string m_rue;
+	System::String^ m_ville;
+	System::String^ m_codePostal;
+	System::String^ m_rue;
+
+public:
+	Adresse();
+	Adresse(int, int, System::String^, System::String^, System::String^);
+	System::String^ SELECT();
+	System::String^ INSERT();
+	System::String^ UPDATE();
+	System::String^ DELETE();
+	int getNumAdresse() { return this->m_numAdresse; };
+	int getReference() { return this->m_reference; };
+	System::String^ getVille() { return this->m_ville; };
+	System::String^ getCodePostal() { return this->m_codePostal; };
+	System::String^ getRue() { return this->m_rue; };
 
 };
