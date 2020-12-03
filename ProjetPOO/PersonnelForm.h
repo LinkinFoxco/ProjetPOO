8706a6b851ba22959a704fc9ba70643ad14665e0
Inès Casenave <ines.ath.casenave@gmail.com>
2020-12-03 15:43:07 +0100

    comboBox lol

--- ProjetPOO/PersonnelForm.h
+++ ProjetPOO/PersonnelForm.h
@@ -18,9 +18,7 @@ namespace ProjetPOO {
 		PersonnelForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
+			
 		}
 
 	protected:
@@ -94,6 +92,10 @@ namespace ProjetPOO {
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::ComboBox^ comboBox1;
 	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label6;
 
 		   String^ mode;
 
@@ -124,16 +126,20 @@ namespace ProjetPOO {
 			this->MessageTxT = (gcnew System::Windows::Forms::Label());
 			this->folderBrowserDialog1 = (gcnew System::Windows::Forms::FolderBrowserDialog());
 			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
 			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label6 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->groupBox1->SuspendLayout();
 			this->SuspendLayout();
@@ -338,32 +344,33 @@ namespace ProjetPOO {
 			this->groupBox1->TabStop = false;
 			this->groupBox1->Text = L"Adresse";
 			// 
-			// label1
+			// textBox4
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(7, 22);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(103, 17);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"Numéro de rue";
+			this->textBox4->Location = System::Drawing::Point(116, 102);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(171, 22);
+			this->textBox4->TabIndex = 7;
 			// 
-			// label2
+			// textBox3
 			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(7, 49);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(82, 17);
-			this->label2->TabIndex = 1;
-			this->label2->Text = L"Nom de rue";
+			this->textBox3->Location = System::Drawing::Point(116, 74);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(171, 22);
+			this->textBox3->TabIndex = 6;
 			// 
-			// label3
+			// textBox2
 			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(7, 77);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(83, 17);
-			this->label3->TabIndex = 2;
-			this->label3->Text = L"Code postal";
+			this->textBox2->Location = System::Drawing::Point(116, 46);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(171, 22);
+			this->textBox2->TabIndex = 5;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(116, 19);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 22);
+			this->textBox1->TabIndex = 4;
 			// 
 			// label4
 			// 
@@ -374,56 +381,97 @@ namespace ProjetPOO {
 			this->label4->TabIndex = 3;
 			this->label4->Text = L"Ville";
 			// 
-			// textBox1
+			// label3
 			// 
-			this->textBox1->Location = System::Drawing::Point(116, 19);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 22);
-			this->textBox1->TabIndex = 4;
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(7, 77);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(83, 17);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Code postal";
 			// 
-			// textBox2
+			// label2
 			// 
-			this->textBox2->Location = System::Drawing::Point(116, 46);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(171, 22);
-			this->textBox2->TabIndex = 5;
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(7, 49);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(82, 17);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Nom de rue";
 			// 
-			// textBox3
+			// label1
 			// 
-			this->textBox3->Location = System::Drawing::Point(116, 74);
-			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(171, 22);
-			this->textBox3->TabIndex = 6;
-			// 
-			// textBox4
-			// 
-			this->textBox4->Location = System::Drawing::Point(116, 102);
-			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(171, 22);
-			this->textBox4->TabIndex = 7;
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(7, 22);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(103, 17);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Numéro de rue";
 			// 
 			// comboBox1
 			// 
 			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(318, 164);
+			this->comboBox1->Location = System::Drawing::Point(27, 378);
 			this->comboBox1->Name = L"comboBox1";
 			this->comboBox1->Size = System::Drawing::Size(190, 24);
 			this->comboBox1->TabIndex = 21;
+			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &PersonnelForm::comboBox1_SelectedIndexChanged);
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(315, 144);
+			this->label5->Location = System::Drawing::Point(24, 358);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(153, 17);
 			this->label5->TabIndex = 22;
 			this->label5->Text = L"Supérieur hiérarchique";
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(273, 380);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(125, 22);
+			this->textBox5->TabIndex = 23;
+			this->textBox5->TextChanged += gcnew System::EventHandler(this, &PersonnelForm::textBox5_TextChanged);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(404, 379);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(85, 23);
+			this->button1->TabIndex = 24;
+			this->button1->Text = L"Ajouter";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &PersonnelForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(495, 378);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(86, 23);
+			this->button2->TabIndex = 25;
+			this->button2->Text = L"Supprimer";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &PersonnelForm::button2_Click);
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(270, 353);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(224, 17);
+			this->label6->TabIndex = 26;
+			this->label6->Text = L"Ajouter ou supprimer un supérieur";
 			// 
 			// PersonnelForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1112, 711);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox5);
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->comboBox1);
 			this->Controls->Add(this->groupBox1);
@@ -491,5 +539,16 @@ namespace ProjetPOO {
 	}
 	private: System::Void vScrollBar1_Scroll(System::Object^ sender, System::Windows::Forms::ScrollEventArgs^ e) {
 	}
+private: System::Void comboBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+	// superieur = comboBox1->GetItemText(comboBox1->SelectedItem->ToString());
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	comboBox1->Items->Add(this->textBox5->Text);
+}
+private: System::Void textBox5_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	comboBox1->Items->Remove(this->comboBox1->Text);
+}
 };
 }
