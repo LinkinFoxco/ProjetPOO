e458e47446402277c9477b02256d0365f75cd9ab
Clémentine COUTANT <clementine.coutant@viacesi.fr>
2020-12-03 11:48:47 +0100

    Classe gestion commande fini

--- ProjetPOO/CL_svc_gestionCommande.cpp
+++ ProjetPOO/CL_svc_gestionCommande.cpp
@@ -22,3 +22,24 @@ int NS_Svc::CL_svc_gestionCommande::ajouter(Article^ article, int qtt, Client^ c
 	reference = cadCommande->actionRowsID(commande->INSERT());
 	return reference;
 }
+
+void NS_Svc::CL_svc_gestionCommande::modifier(int referenceCommande, Article^ article, int qtt, Client^ client, moyenDePaiement^ moyPaiement, String^ dateLivraison, String^ dateEmission, String^ datePaiement)
+{
+	commande->modifierReferenceCommande(referenceCommande);
+	commande->ajouterArticles(article, qtt);
+	commande->calculPanier();
+	commande->modifierMoyenDePaiement(moyPaiement);
+	commande->modifierClient(client);
+	commande->modifierDateEmission(dateEmission);
+	commande->modifierDateLivraison(dateLivraison);
+	commande->modifierDatePaiement(datePaiement);
+
+	cadCommande->actionRows(commande->UPDATE());
+}
+
+void NS_Svc::CL_svc_gestionCommande::supprimer(int referenceCommande)
+{
+	commande->modifierReferenceCommande(referenceCommande);
+
+	cadCommande->actionRows(commande->DELETE());
+}
--- ProjetPOO/CL_svc_gestionCommande.h
+++ ProjetPOO/CL_svc_gestionCommande.h
@@ -21,23 +21,7 @@ namespace NS_Svc
 		};
 		DataSet^ listeCommande(String^);
 		int ajouter(Article^ article, int qtt, Client^ client, moyenDePaiement^ moyPaiement, String^ dateLivraison, String^ dateEmission, String^ datePaiement);
-		void modifier();
+		void modifier(int referenceCommande, Article^ article, int qtt, Client^ client, moyenDePaiement^ moyPaiement, String^ dateLivraison, String^ dateEmission, String^ datePaiement);
 		void supprimer(int);
 	};
 }
-/*
-int referenceCommande;
-	int m_totalArticle;
-	int m_totalPrixHT;
-	int m_totalPrixTVA;
-	int m_totalPrixTTC;
-	List<qttArticle^> m_articles;
-	System::String^ m_2Lprenom;
-	System::String^ m_2Lnom;
-	System::String^ m_3Lville;
-	System::String^ m_dateLivraison;
-	System::String^ m_dateEmission;
-	System::String^ m_datePaiement;
-	moyenDePaiement m_moyenPaiement;
-	Client^ m_client;
-*/
